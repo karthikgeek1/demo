b86ff59f5c30c45ace6c872d8e8bd27c7cb5f989
karthik kjs <104209441+karthikgeek1@users.noreply.github.com>
2023-02-14 11:28:00 +0530

    check 1

---
+++ Hello.js
@@ -0,0 +1,9 @@
+const myname = 'karthik'
+
+console.log('hello')
+
+function add (a,b){
+    return a+b
+}
+
+module.exports = add
---
+++ msg.js
@@ -0,0 +1,8 @@
+const add = require('./Hello.js')
+const validator = require('validator')
+const chalk = require('chalk')
+
+console.log(add(2,3))
+
+console.log(chalk.bgGreen.bold('success'))
+
